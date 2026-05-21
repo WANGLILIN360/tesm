09f041ba285974dcc101735d2dcdab5a7b7fa14a
北京脉象机器人智能科技有限公司 <wanglilin@qz.cool>
2026-05-21 12:55:35 +0800

    Add tesm_ssm/ops/triton/__init__.py

---
+++ tesm_ssm/ops/triton/__init__.py
@@ -0,0 +1,53 @@
+from tesm_ssm.ops.triton.tesm_kernels import (
+    tesm_triton_is_available,
+    # Forward kernels (inference)
+    triton_chunk_state_scan,
+    triton_fused_output_combine,
+    triton_local_entanglement,
+    triton_quantized_linear,
+    # Autograd versions (training)
+    triton_chunk_state_scan_autograd,
+    triton_quantized_linear_autograd,
+    triton_local_entanglement_autograd,
+    triton_fused_output_combine_autograd,
+)
+
+from tesm_ssm.ops.triton.global_entanglement import (
+    triton_global_entanglement,
+)
+
+from tesm_ssm.ops.triton.tesm_mimo_kernel import (
+    # MIMO kernels
+    tesm_state_scan_triton,
+    tesm_state_scan_triton_autograd,
+    tesm_local_entanglement_triton,
+    tesm_local_entanglement_triton_autograd,
+    tesm_mimo_fused_triton,
+    # MIMO Global Entanglement
+    tesm_global_entanglement_mimo_triton,
+    tesm_global_entanglement_mimo_triton_autograd,
+)
+
+__all__ = [
+    "tesm_triton_is_available",
+    # Forward kernels
+    "triton_chunk_state_scan",
+    "triton_fused_output_combine",
+    "triton_local_entanglement",
+    "triton_quantized_linear",
+    "triton_global_entanglement",
+    # Autograd versions
+    "triton_chunk_state_scan_autograd",
+    "triton_quantized_linear_autograd",
+    "triton_local_entanglement_autograd",
+    "triton_fused_output_combine_autograd",
+    # MIMO kernels
+    "tesm_state_scan_triton",
+    "tesm_state_scan_triton_autograd",
+    "tesm_local_entanglement_triton",
+    "tesm_local_entanglement_triton_autograd",
+    "tesm_mimo_fused_triton",
+    # MIMO Global Entanglement
+    "tesm_global_entanglement_mimo_triton",
+    "tesm_global_entanglement_mimo_triton_autograd",
+]
